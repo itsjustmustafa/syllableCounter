51d91e9472961091a1bf194adf0766799eb09ab3
Mustafa Barodawala <itsjustmustafa@gmail.com>
2017-10-05 16:02:59 +1100

    Updated syllables.py
    Added a bit of extra decision making!

--- syllables.py
+++ syllables.py
@@ -2,7 +2,24 @@ def syl(word):#a word is a string containing ONLY alphabetical characters
     vow = 'a e i o u y'.split(' ')
     syl = 0
     for i in range(len(word)):
+        print(word[i]+":")
+        
         if(word[i] in vow):
-            syl+=1
+            print("    is a vowel")
+            
+            if(i<len(word)-1):
+                print("    not last letter")
+                
+                if(word[i+1] in vow):
+                    print("    next letter is vowel")
+                else:
+                    print("    next letter is not vowel")
+                    print("    ADD SYL")
+                    syl+=1
+            else:
+                print("    last letter")
+                print("    ADD SYL")
+                syl+=1
+            
             
     return(syl)
